8ac0195a3c253bbe6977ad900d344d0417dae507
Corbanezi <feipe_corbanezi@hotmail.com>
2022-02-28 11:00:27 -0300

    Just a Test

---
+++ teste.js
@@ -0,0 +1,43 @@
+var valor
+var valor1
+var oper
+var readlineSync = require('readline-sync');
+oper = readlineSync.question("Qual operacao deseja efetuar (+) (-) () (/)? : \n");
+valor = parseFloat(readlineSync.question("Insira o primeiro numero: \n"));
+valor1 = parseFloat(readlineSync.question("Insira o segundo numero: \n"));
+
+x = 13;
+
+let haj = false;
+
+if(false) {
+    return 12;
+}
+
+jin = false;
+
+array.forEach(element => {
+
+});
+
+
+
+function doOperation(operator, value1, value2) {
+    if (operator == "+") {
+        return value1 + value2;
+    } else if
+        (operator == "-") {
+        return value1 - value2;
+    } else if
+        (operator == "") {
+        return value1 * value2;
+    } else if
+        (operator == "/") {
+        return value1 / value2;
+    } else {
+        throw new Error('Operação inválida');
+    }
+}
+
+
+console.log('O resultado é', doOperation(oper, valor, valor1))
